0807d7b5a50b0145f1768ddc6b89b0dd38f3e375
Chris MacLellan <cm3786@drexel.edu>
2020-12-22 11:49:05 -0500

    got the dual ppo working, with some of the classes for optuna tuning"

--- sandbox/multicolumn/run_dual_ppo.py
+++ sandbox/multicolumn/run_dual_ppo.py
@@ -1,8 +1,13 @@
+from typing import Dict
+from typing import Any
 import numpy as np
 import gym
 from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.vec_env import VecEnv
+import optuna
 
 from tutorenvs.utils import MultiDiscreteToDiscreteWrapper
 
