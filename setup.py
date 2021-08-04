4cff2f19939c519adf7caf935e0e64f8bbede454
ag3338 <ag3338@cs.drexel.edu>
2021-08-04 17:42:10 -0400

    changing version for error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tutorgym',
-    version='0.0.1',    
+    version='0.0.2',    
     description='A toolkit that provides a machine interfaces for multiple tutor environments. TutorGym leverages the OpenAI Gym to enable existing RL implementations (that support Gym) to interface with these environments',
     url='https://gitlab.cci.drexel.edu/teachable-ai-lab/tutorenvs',
     author='Christopher J. MacLellan',
@@ -17,7 +17,7 @@ setup(
     classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Science/Research',
-    'topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
     'License :: OSI Approved :: MIT License',  
     'Operating System :: OS Independent',        
     'Programming Language :: Python',
